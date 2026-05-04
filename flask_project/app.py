dab46b5b9f74836a3ef0ea59f7e99e50297a586f
ConnorMcKay1 <mckayc1@go.stockton.edu>
2026-05-04 13:34:01 -0400

    got the structure set up. Going to start working on bringing in concrete predictor

--- flask_project/app.py
+++ flask_project/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Route for the homepage
 @app.route("/")
 def home():
     # This will render the index.html file from the templates folder
@@ -14,23 +15,22 @@ def structural():
     return render_template("structural.html")
 
 
+# Route for the Thermal Dynamics page
+@app.route("/thermal")
+def thermal():
+    return render_template("thermal.html")
 
 
+# Route for the Fluid Dynamics page
+@app.route("/fluid")
+def fluid():
+    return render_template("fluid.html")
 
-# # Route for the Thermal Dynamics page
-# @app.route("/thermal")
-# def thermal():
-#     return render_template("thermal.html")
 
-# # Route for the Fluid Dynamics page
-# @app.route("/fluid")
-# def fluid():
-#     return render_template("fluid.html")
-
-# # Route for the Concrete prediction page
-# @app.route("/concrete")
-# def concrete():
-#     return render_template("concrete.html")
+# Route for the Concrete prediction page
+@app.route("/concrete")
+def concrete():
+    return render_template("concrete.html")
 
 
 
